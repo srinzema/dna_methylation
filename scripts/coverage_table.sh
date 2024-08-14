0848342ede0aa6768441a405942c2a827e9ed91e
srinzema <srinzema@users.noreply.github.com>
2024-08-14 09:55:52 +0200

    Made a rule that runs "coverage_summary.py" called "coverage_summary". Renamed the original "coverage_summary" rule to "coverage_table". This finishes the last task of issue #3

--- scripts/coverage_table.sh
+++ scripts/coverage_table.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
 # Check if enough arguments were provided
-if [ $# -lt 3 ]; then
-  echo "Usage: $0 <summary_output_file> <input_file1> <input_file2> ... <input_fileN>"
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <input_file> <output_file>"
   exit 1
 fi
 
-# Assign the first argument to the output file
-output_file=$1
-shift
+# Assign the first and second arguments to variables
+input_file=$1
+output_file=$2
 
-for file in "$@"; do
-    sample_name="$(basename $file .coverage.summary.txt)"
-    echo $sample_name
-    awk -v sample="$sample_name" 'NR==1 {next} {print $1, $4}' "$file" | 
-    awk -v fname="$file" '{print $1, $2 "\t" fname}' fname="$sample_name" > "${output_file}"
-done
+# Process the input file with awk and write to the output file
+awk -v outfile="$output_file" '
+BEGIN { 
+  # Open the output file for writing
+  print "chromosome\tmethylated\tunmethylated\tpercentage" > outfile;
+}
+
+{ 
+  # Accumulate totals for all chromosomes
+  total_methylated += $4;
+  total_unmethylated += $5;
+
+  # Collect sums for each unique chromosome
+  methylated_by_chr[$1] += $4;
+  unmethylated_by_chr[$1] += $5;
+}
+
+END {
+  total = total_methylated + total_unmethylated;
+  
+  # Print totals for all chromosomes
+  printf "Total\t%d\t%d\t%.2f%%\n", total_methylated, total_unmethylated, (total_methylated / total) * 100 >> outfile;
+
+  # Print results per chromosome in alphabetical order
+  PROCINFO["sorted_in"] = "@ind_str_asc";
+  for (chr in methylated_by_chr) {
+    total_by_chr = methylated_by_chr[chr] + unmethylated_by_chr[chr];
+    if (total_by_chr > 0) {
+      printf "%s\t%d\t%d\t%.2f%%\n", chr, methylated_by_chr[chr], unmethylated_by_chr[chr], (methylated_by_chr[chr] / total_by_chr) * 100 >> outfile;
+    } else {
+      printf "%s\t%d\t%d\t0.0\n", chr, methylated_by_chr[chr], unmethylated_by_chr[chr] >> outfile;
+    }
+  }
+}' "$input_file"
